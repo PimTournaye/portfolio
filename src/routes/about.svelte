f35ba96ef311157e9f948346f1fcf0778d2a8cc0
PimTournaye <pimtournaye@student.ehb.be>
2022-03-01 14:16:23 +0100

    init

---
+++ src/routes/about.svelte
@@ -0,0 +1,16 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+
+<section>
+	<h1 class="bg-slate-500 from-teal-500">Quick introduction</h1>
+
+	<picture>
+		<source />
+	</picture>
+</section>
