10761ffa4491729abc0bc031a1869b523dcb654f
PimTournaye <pim.tournaye@student.ehb.be>
2022-08-12 18:55:05 +0200

    oof

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -1,27 +1,20 @@
 <script context="module" lang="ts">
-	import Projects from '$lib/components/Projects.svelte';
-
-	import { browser } from '$app/env';
-	import { GQL_Projects } from '$houdini'
+	import { GQL_Projects } from '$houdini';
+    import Projects from '$lib/components/Projects.svelte';
 	import type { LoadEvent } from '@sveltejs/kit';
 
-	export const router = browser;
-
-	export const load = async (event: LoadEvent) => {
+	export async function load(event: LoadEvent) {
 		await GQL_Projects.fetch({ event });
 		return {};
 	}
 </script>
 
 <svelte:head>
-	<title>Projects</title>
+    <title>Projects</title>
 </svelte:head>
 
 <section>
-	<h1 class="title text-4xl mb-10 font-bold">Projects</h1>
-	<div>Here are some noteworthy projects I've done in the past.</div>
-</section>
-
-<section class="flex md:flex-row sm:flex-col justify-between">
-	<Projects />
+    <h1 class="title text-4xl mb-10 font-bold">Projects</h1>
+    <div>Here are some noteworthy projects I've done in the past.</div>
+    <Projects />
 </section>
