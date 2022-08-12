b82d6d982c22739ce8563f98a10ad40f1428b2d0
PimTournaye <pim.tournaye@student.ehb.be>
2022-08-12 15:17:15 +0200

    deleting old stuff

--- src/lib/graphql-client.js
+++
@@ -1,7 +0,0 @@
-import { GraphQLClient } from 'graphql-request';
-const URL = import.meta.env.VITE_GRAPHQL_URL;
-const TOKEN = import.meta.env.VITE_SECRET_TOKEN;
-
-export const client = new GraphQLClient(URL, {
-    headers: { Autherization: `Bearer ${TOKEN}` }
-});
--- src/routes/projects/[slug].svelte
+++ src/routes/projects/[slug].svelte
@@ -1,19 +1,18 @@
-<script context="module">
-	import tagColor from '$lib/tags-colors';
+<script lang="ts">
+	import { GQL_Project } from '$houdini';
+	import type { LoadEvent } from '@sveltejs/kit';
 
-	export const load = async ({ fetch, params }) => {
-		const { slug } = params;
-		const res = await fetch(`/projects/${slug}.json`);
-		if (res.ok) {
-			const { project } = await res.json();
-			return { props: { project } };
-		}
-	};
-</script>
+	export async function load(event: LoadEvent) {
+		const { slug } = event.params;
+		await GQL_Project.fetch({ event, variables: { slug } });
+		return {};
+	}
 
-<script>
-	export let project;
-	const { name, image, description, tags } = project;
+	const { name, description} = $GQL_Project.data?.project || {};
+	const tags = $GQL_Project.data?.project?.tags;
+	const image = $GQL_Project.data?.project?.image[0].url;
+	const demo = $GQL_Project.data?.project?.demo;
+	const source = $GQL_Project.data?.project?.sourceCode;
 </script>
 
 <svelte:head>
@@ -23,26 +22,26 @@
 <section>
 	<h1>{name}</h1>
 	<figure class="px-10 pt-10">
-		<img src={image[0].url} alt="Picture of {name}" />
+		<img src={image} alt="Picture of {name}" />
 	</figure>
-	{#each tags as tag}
+	<!-- {#each tags as tag}
 		<div class="badge badge-outline">
-			<span class="text=[{tagColor(tag)}] mr-2 mb-0.5">●</span><span>{tag}</span>
+			<span>{tag}</span>
 		</div>
-	{/each}
+	{/each} -->
 	<p>{description}</p>
 
-	{#if project.demo}
+	{#if demo}
 		<div class="mt-5">
-			<a href={project.demo} class="btn btn-primary">
+			<a href={demo} class="btn btn-primary">
 				<span class="text-base-content">Demo</span>
 			</a>
 		</div>
 	{/if}
 
-	{#if project.sourceCode}
+	{#if source}
 		<div class="mt-5">
-			<a href={project.sourceCode} class="btn btn-primary">
+			<a href={source} class="btn btn-primary">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					class="h-5 w-5"
