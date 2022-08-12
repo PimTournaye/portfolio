e5251522c40083e3923491a2404df10790e1c491
PimTournaye <pim.tournaye@student.ehb.be>
2022-08-12 21:32:07 +0200

    config stuff

--- houdini.config.js
+++ houdini.config.js
@@ -3,6 +3,7 @@ const config = {
     "schemaPath": "./schema.graphql",
     "sourceGlob": "src/**/*.{svelte,gql,graphql}",
     "apiUrl": process.env.VITE_GRAPHQL_URL
+    //"apiUrl": import.meta.env.VITE_GRAPHQL_URL
 }
 
 export default config;
