f6bb65a27ca3c1248a81625b7ee6d31356e9ad48
PimTournaye <pim.tournaye@student.ehb.be>
2022-08-29 12:12:10 +0200

    Projects rendering again

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -3,6 +3,8 @@
 	export let data;
 	$: ({ Projects } = data);
 
+		console.log($Projects);
+
 </script>
 
 <svelte:head>
@@ -13,8 +15,7 @@
 	<h1 class="title text-4xl mb-10 font-bold">Projects</h1>
 	<div>Here are some noteworthy projects I've done in the past.</div>
 
-	{$Projects?.data?.projects}
-	<!-- {#each Projects as { name, slug, short, image, year, tags }}
+	{#each $Projects.data.projects as { name, slug, short, image, year, tags }}
 		<ProjectCard {name} {slug} {short} images={image} {year} {tags} />
-	{/each} -->
+	{/each}
 </section>
