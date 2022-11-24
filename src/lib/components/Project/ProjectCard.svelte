071c706b666e97fe27239157809264a8ed36fa0a
PimTournaye <pim.tournaye@student.ehb.be>
2022-11-24 11:25:12 +0100

    updated components

--- src/lib/components/Project/ProjectCard.svelte
+++ src/lib/components/Project/ProjectCard.svelte
@@ -5,22 +5,27 @@
 	export let year;
 	export let images;
 	export let tags;
+
+	// if images is a an array, take the first image url
+	// otherwise, take the image url
+	let image = Array.isArray(images) ? images[0] : images;
+	let imageUrl = image.url;
 </script>
 
 <a href="/projects/{slug}" class="flex flex-col items-center
  bg-white rounded-lg border shadow-md mb-8
  md:flex-row md:max-w-xl 
  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-	<div class="flex flex-col justify-between p-4 leading-normal">
+	<div class="flex flex-col justify-between p-4 leading-normal w-full">
 		<img
-		class="w-full object-cover object-center transition-all ease-in duration-200 max-h-60 hover:scale-105 bg-gray-50 md:h-36 lg:h-60"
-		src={images[0].url}
+		class="max-w-full max-h-60 object-cover object-center transition-all ease-in duration-200 hover:scale-105 bg-gray-50 md:h-36 lg:h-60"
+		src={imageUrl}
 		alt="Picture of {title}"
 		/>
 		<h5 class="mt-4 mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
 		<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{short}</p>
 		{#if tags}
-		<div class="mt-4 space-x-2">
+		<div class="space-x-2">
 			{#each tags as tag}
 			<div class="pill bg-gray-400 rounded-full text-xs px-4 py-1 mb-2 inline-block">
 				{tag}
--- src/lib/components/SkillIcon.svelte
+++ src/lib/components/SkillIcon.svelte
@@ -1,19 +1,42 @@
 <script>
-  export let level ;
-  export let svg = '';
-  export let text = '';
-  export let name = '';
+	export let level;
+	export let icon = '';
+	export let text = '';
+	export let name = '';
+
+	// TODO: add dark mode colors
+	let color = () => {
+		if (level > 70) {
+			return 'text-bright-bau-red';
+		} else if (level > 50) {
+			return 'text-bright-bau-yellow';
+		} else {
+			return 'text-bright-bau-blue';
+	};
+}
 </script>
 
-<div class="flex group">
-  <div class="radial-progress" style="--value:{level}">
-    <img src={svg} alt="A programming icon of {name}" />
+<div class="flex flex-col w-1/4 flex-grow mx-4 mb-8 justify-center max-w-lg
+ bg-slate-100 px-8 py-8">
+	<div
+		class="radial-progress bg-gray-300 {color()} shadow-lg
+          mx-auto mb-6"
+		style="--value:{level}; --thickness: 4px"
+	>
+		<img src={icon.url} alt="icon of {name}" class="h-10 w-10" />
+	</div>
+	<div class="flex flex-col flex-grow space-y-2 mx-auto">
+    <h1 class="font-bold text-lg">{name}</h1>
+    <p
+      class="
+          text-sm
+          text-gray-500
+          font-light
+          leading-relaxed
+          text-justify
+          max-w-xs"
+    >
+      {text}
+    </p>
   </div>
-  <p
-    class="
-    transition-all duration-100 scale-0
-    group-hover:scale-100"
-  >
-    {text}
-  </p>
 </div>
