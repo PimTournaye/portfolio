850a69792c520b7319f2a932c59df468952c74c1
PimTournaye <pim.tournaye@student.ehb.be>
2023-07-22 18:40:27 -0400

    refactor init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang='ts'>
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+</script>
+
+<AppShell>
+	<svelte:fragment slot="sidebarLeft">Help</svelte:fragment>
+
+	<main>
+		<slot />
+	</main>
+
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+</AppShell>
---
+++ tailwind.config.cjs
@@ -0,0 +1,34 @@
+import { join } from 'path'
+import typography from '@tailwindcss/typography'
+import skeleton from '@skeletonlabs/skeleton/tailwind/skeleton.cjs'
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	darkMode: 'class',
+	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	theme: {
+    extend: {
+      colors: {
+        'bau-red': '#BE1E2D',
+        'bau-blue': '#21409A',
+        'bau-yellow': '#FFDE17',
+        'bright-bau-red': '#e83c28',
+        'bright-bau-blue': '#091c44',
+        'bright-bau-yellow': '#f4b800',
+        'timberwolf': '#DBD4D3',
+        nyu: {
+          '300': '#330662',
+          '400': '#702b9d',
+          '500': '#7b5aa6',
+          '600': '#ab82c5',
+          '700': '#eee6f3',
+        },
+      },
+      borderRadius: ['hover']
+    },
+    borderRadius: {
+      full: '200px'
+    }
+  },
+	plugins: [typography,...skeleton()],
+}
